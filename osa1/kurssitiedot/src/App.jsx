0803789037736ac4bf54272f4a4305b89db19bb3
LeoLampio <149969862+LeoLampio@users.noreply.github.com>
2026-01-19 22:47:14 +0200

    OSA 2 teht 1

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,58 +1,31 @@
-
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      <Part name={parts[0].name} count={parts[0].exercises} />
-      <Part name={parts[1].name} count={parts[1].exercises} />
-      <Part name={parts[2].name} count={parts[2].exercises} />
-    </>
-  )
-}
-
-const Part = ({ name, count }) => {
-  return (
-    <p>
-      {name} {count}
-    </p>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
