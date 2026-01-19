e8889af21e79699ff985151113ac110f6eec9401
LeoLampio <149969862+LeoLampio@users.noreply.github.com>
2026-01-19 23:15:01 +0200

    OSA 2 teht 6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(p => <p key={p.name}>{p.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
