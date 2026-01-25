d78e610338a3a4cc2019df89c9425eb91f9648b4
LeoLampio <149969862+LeoLampio@users.noreply.github.com>
2026-01-25 19:22:41 +0200

    OSA 3 teht 2

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -24,12 +24,19 @@ let persons = [
   }
 ]
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  let date = new Date();
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date.toLocaleDateString("fi-FI")} ${date.toLocaleTimeString("fi-FI")}</p>`);
 })
 
 const PORT = 3001
